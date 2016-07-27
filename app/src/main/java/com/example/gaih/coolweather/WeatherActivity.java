4f87da765c17fd3666550be14773f3c51f37b56f
Gai <gaih@outlook.com>
2016-07-27 11:15:10 +0800

    手动切换天气以及更新

--- app/src/main/java/com/example/gaih/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/gaih/coolweather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.example.gaih.coolweather;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -8,6 +10,7 @@ import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,7 +24,7 @@ import org.w3c.dom.Text;
  * Created by gaih on 2016/7/26.
  */
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements View.OnClickListener {
     private LinearLayout weatherInfoLayout;
     private TextView cityNameText;
     private TextView publishText;
@@ -29,6 +32,9 @@ public class WeatherActivity extends Activity {
     private TextView temp1Text;
     private TextView temp2Text;
     private TextView currentDateText;
+
+    private Button switchCity;
+    private Button refreshWeather;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,12 @@ public class WeatherActivity extends Activity {
         temp1Text = (TextView) findViewById(R.id.temp1);
         temp2Text = (TextView) findViewById(R.id.temp2);
         currentDateText = (TextView) findViewById(R.id.current_date);
+
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
+
         String countryCode = getIntent().getStringExtra("country_code");
         if(!TextUtils.isEmpty(countryCode)){
             publishText.setText("同步中....");
@@ -53,6 +65,28 @@ public class WeatherActivity extends Activity {
             showWeather();
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.switch_city:
+                Intent intent = new Intent(this,ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity",true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                publishText.setText("同步中......");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode = prefs.getString("weather_code","");
+                if(!TextUtils.isEmpty(weatherCode)){
+                    queryWeatherInfo(weatherCode);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     private void queryWeatherCode(String countryCode) {
